d5dbb423f6080b8a4675d06d47e57976edd4d2f8
ArtsemKos <Artseem.k@gmail.com>
2024-03-29 11:06:05 +0300

    HW_DYN_Form

--- DYN_FORM/script.js
+++ DYN_FORM/script.js
@@ -29,38 +29,92 @@ const formDef2 =
 
 function createForm(name, arr) {
   arr.forEach(element => {
-    
-      const lab = document.createElement('label');
-      lab.innerHTML = element.label||'';
 
-      name.appendChild(lab);
-    
+    const lab = document.createElement('label');
+    lab.innerHTML = element.label || '';
+
+    name.appendChild(lab);
+
     const br = document.createElement('br');
     const inp = document.createElement('input');
+    const sel = document.createElement('select');
+    const text = document.createElement('textarea');
+
+
     switch (element.kind) {
       case 'longtext':
         inp.type = 'text';
+        inp.name = element.name;
         inp.size = '50';
         lab.appendChild(inp);
         lab.appendChild(br);
         break;
       case 'number':
         inp.type = 'number';
+        inp.name = element.name;
         inp.size = '10';
         lab.appendChild(inp);
         lab.appendChild(br);
         break;
       case 'submit':
         inp.type = 'submit';
+        inp.name = element.name;
         inp.value = element.caption;
         lab.appendChild(inp);
         lab.appendChild(br);
         break;
+      case 'shorttext':
+        inp.type = 'email';
+        inp.name = element.name;
+        inp.size = '10';
+        lab.appendChild(inp);
+        lab.appendChild(br);
+        break;
+
+      case 'combo':
+        sel.name = lab.name
+        lab.appendChild(sel);
+        element.variants.forEach(elements => {
+          const opt = document.createElement('option');
+          opt.name = elements.name;
+          opt.innerHTML = elements.text
+          opt.value = elements.value;
+          sel.appendChild(opt);
+        })
+        lab.appendChild(br);
+        break;
+
+      case 'radio':
+        element.variants.forEach(elements => {
+          const radio = document.createElement('input');
+          const span = document.createElement('span')
+
+          radio.type = 'radio';
+          radio.name = 'radio';
+          span.innerHTML = elements.text;
+
+          lab.appendChild(radio);
+          lab.appendChild(span)
+        })
+        lab.appendChild(br);
+        break;
+
+      case 'check':
+        inp.type = 'checkbox';
+        inp.name = element.name;
+        lab.appendChild(inp);
+        lab.appendChild(br);
+        break;
+
+      case 'memo':
+       text.name = element.name;
+        lab.appendChild(text);
+        // lab.insertBefore(br,text)
+        lab.appendChild(br);
+        break;
     }
-
-
   });
 
-
 }
-createForm(document.forms.form2, formDef1)
+createForm(document.forms.form1, formDef1);
+createForm(document.forms.form2, formDef2);
